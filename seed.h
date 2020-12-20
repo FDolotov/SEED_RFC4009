888e23e71ff28c1d80f1befc1f2dd2463c2c808b
SirJorah <59660051+FDolotov@users.noreply.github.com>
2020-12-20 15:42:13 +0300

    Update seed.h

--- seed.h
+++ seed.h
@@ -10,13 +10,10 @@ struct key
 	u_int32_t ks[32];
 };
 
-//error_t seedInit(SeedContext *context, u_int8_t key, size_t key);
-
-void seed_encrypt(struct key*, const u_int32_t [], u_int32_t []);//, u_int64_t kLv, u_int64_t kRv);
+void seed_encrypt(struct key*, const u_int32_t [], u_int32_t []);
 void seed_decrypt(struct key*, const u_int32_t [], u_int32_t []);
 
-//u_int32_t G(u_int32_t);
-//u_int64_t F(u_int64_t, u_int32_t, u_int32_t);
-void key_schedule(struct key*, u_int32_t []);//, u_int64_t);
+void key_schedule_set(struct key*, u_int32_t []);
+void key_schedule_release(struct key*);
 
 #endif
