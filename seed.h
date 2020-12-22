dacd55f18569a85edad89ab246780e353865df18
SirJorah <59660051+FDolotov@users.noreply.github.com>
2020-12-22 17:39:00 +0300

    Update seed.h

--- seed.h
+++ seed.h
@@ -14,6 +14,5 @@ void seed_encrypt(struct key*, const u_int32_t [], u_int32_t []);
 void seed_decrypt(struct key*, const u_int32_t [], u_int32_t []);
 
 void key_schedule_set(struct key*, u_int32_t []);
-void key_schedule_release(struct key*);
 
 #endif
